6dae2a155250ebd1e98796da0a514d0716567222
mootoh <mootoh@gmail.com>
2009-03-06 18:08:43 +0900

    add TaskView in the scaffold.

---
+++ scaffold/ScaffoldTaskViewController.h
@@ -0,0 +1,15 @@
+//
+//  ScaffoldTaskViewController.h
+//  Milpon
+//
+//  Created by mootoh on 3/6/09.
+//  Copyright 2009 deadbeaf.org. All rights reserved.
+//
+
+@class TaskProvider;
+
+@interface ScaffoldTaskViewController : UITableViewController {
+   TaskProvider *tp;
+}
+
+@end
---
+++ scaffold/ScaffoldTaskViewController.m
@@ -0,0 +1,149 @@
+//
+//  ScaffoldTaskViewController.m
+//  Milpon
+//
+//  Created by mootoh on 3/6/09.
+//  Copyright 2009 deadbeaf.org. All rights reserved.
+//
+
+#import "ScaffoldTaskViewController.h"
+#import "TaskProvider.h"
+#import "RTMTask.h"
+#import "logger.h"
+
+@implementation ScaffoldTaskViewController
+
+/*
+- (id)initWithStyle:(UITableViewStyle)style {
+    // Override initWithStyle: if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+    if (self = [super initWithStyle:style]) {
+    }
+    return self;
+}
+*/
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    tp = [[TaskProvider sharedTaskProvider] retain];
+}
+
+/*
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+*/
+/*
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+*/
+/*
+- (void)viewWillDisappear:(BOOL)animated {
+	[super viewWillDisappear:animated];
+}
+*/
+/*
+- (void)viewDidDisappear:(BOOL)animated {
+	[super viewDidDisappear:animated];
+}
+*/
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
+}
+
+#pragma mark Table view methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+   return 1;
+}
+
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+   LOG(@"tp tasks count = %d", [tp tasks].count);
+   return [tp tasks].count;
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+   static NSString *CellIdentifier = @"ScaffoldTaskViewCell";
+
+   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+   if (cell == nil) {
+      cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
+   }
+
+   RTMTask *task = (RTMTask *)[[tp tasks] objectAtIndex:indexPath.row];
+   cell.text = [NSString stringWithFormat:@"%d: %@", [task.iD intValue], task.name];
+
+   return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+   // Navigation logic may go here. Create and push another view controller.
+   // AnotherViewController *anotherViewController = [[AnotherViewController alloc] initWithNibName:@"AnotherView" bundle:nil];
+   // [self.navigationController pushViewController:anotherViewController];
+   // [anotherViewController release];
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+// Return NO if you do not want the specified item to be editable.
+return YES;
+}
+*/
+
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+
+if (editingStyle == UITableViewCellEditingStyleDelete) {
+// Delete the row from the data source
+[tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
+}   
+else if (editingStyle == UITableViewCellEditingStyleInsert) {
+// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+}   
+}
+*/
+
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+// Return NO if you do not want the item to be re-orderable.
+return YES;
+}
+*/
+
+
+- (void)dealloc {
+   [super dealloc];
+}
+
+
+@end
+
