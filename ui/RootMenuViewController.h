203fd37c76384c6eba287b7072ed5d99b27b2c0b
mootoh <mootoh@gmail.com>
2009-04-12 16:11:14 +0900

    add RootMenuViewController, but not linked yet.

---
+++ ui/RootMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootMenuViewController.h
+//  Milpon
+//
+//  Created by Motohiro Takayama on 4/12/09.
+//  Copyright 2009 deadbeaf.org. All rights reserved.
+//
+
+@interface RootMenuViewController : UITableViewController
+{
+}
+
+@end
