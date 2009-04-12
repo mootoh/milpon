898a1ad41aa20af1289b4e9805df7abf469a110a
mootoh <mootoh@gmail.com>
2009-04-12 21:20:50 +0900

    add ReviewViewController.

---
+++ ui/ReviewViewController.h
@@ -0,0 +1,16 @@
+//
+//  ReviewViewController.h
+//  Milpon
+//
+//  Created by mootoh on 4/12/09.
+//  Copyright 2009 deadbeaf.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ReviewViewController : UITableViewController {
+
+}
+
+@end
