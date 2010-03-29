688ab03e611887ac2212bd43ddd8b5e13ab042c1
mootoh <mootoh@gmail.com>
2010-03-29 23:00:31 +0900

    added InvoView.

---
+++ ui/InfoViewController.h
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.h
+//  Milpon
+//
+//  Created by Motohiro Takayama on 3/29/10.
+//  Copyright 2010 deadbeaf.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface InfoViewController : UITableViewController {
+
+}
+
+@end
