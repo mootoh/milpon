622239d569cb2851f93e9dbc879cf27ec5c92329
mootoh <mootoh@gmail.com>
2010-07-10 18:59:41 +0900

    trivial.

--- ui/MPAppDelegate.m
+++ ui/MPAppDelegate.m
@@ -66,12 +66,12 @@
  Returns the managed object context for the application.
  If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
  */
-- (NSManagedObjectContext *) managedObjectContext {
-   
+- (NSManagedObjectContext *) managedObjectContext
+{
    if (managedObjectContext != nil) {
       return managedObjectContext;
    }
-   
+
    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
    if (coordinator != nil) {
       managedObjectContext = [[NSManagedObjectContext alloc] init];
@@ -80,13 +80,12 @@
    return managedObjectContext;
 }
 
-
 /**
  Returns the managed object model for the application.
  If the model doesn't already exist, it is created by merging all of the models found in the application bundle.
  */
-- (NSManagedObjectModel *)managedObjectModel {
-   
+- (NSManagedObjectModel *)managedObjectModel
+{
    if (managedObjectModel != nil) {
       return managedObjectModel;
    }
@@ -94,19 +93,18 @@
    return managedObjectModel;
 }
 
-
 /**
  Returns the persistent store coordinator for the application.
  If the coordinator doesn't already exist, it is created and the application's store added to it.
  */
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
-   
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
    if (persistentStoreCoordinator != nil) {
       return persistentStoreCoordinator;
    }
-   
+
    NSURL *storeUrl = [NSURL fileURLWithPath: [[self applicationDocumentsDirectory] stringByAppendingPathComponent: @"Milpon.sqlite"]];
-   
+
    NSError *error = nil;
    persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
    if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeUrl options:nil error:&error]) {
@@ -127,17 +125,16 @@
    return persistentStoreCoordinator;
 }
 
-
 #pragma mark -
 #pragma mark Application's Documents directory
 
 /**
  Returns the path to the application's Documents directory.
  */
-- (NSString *)applicationDocumentsDirectory {
+- (NSString *)applicationDocumentsDirectory
+{
    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
 }
-
 
 #pragma mark -
 #pragma mark Memory management
